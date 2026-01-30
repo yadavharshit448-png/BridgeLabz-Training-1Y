19f1aeb67215f3e27e58c214dc8f90b5a5cf94f0
yadavharshit448-png <yadavharshit118@gmail.com>
2026-01-30 11:34:26 +0530

    added some files

--- practice-programs/ArraySearch.java
+++ practice-programs/ArraySearch.java
@@ -1,34 +1,23 @@
 import java.util.Scanner;
-
-class ArraySearch {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int[] marks = {12, 14, 16, 17, 18};
-
-        // Print array elements
-        for (int i = 0; i < marks.length; i++) {
-            System.out.println(marks[i]);
-        }
-
-        System.out.println("Enter your element:");
-        int m = sc.nextInt();
-
-        boolean found = false;
-
-        for (int i = 0; i < marks.length; i++) {
-            if (marks[i] == m) {
-                System.out.println("Number found at index: " + i);
-                found = true;
-                break;   // stop after finding
-            }
-        }
-
-        if (!found) {
-            System.out.println("Number not found.");
-        }
-
-        sc.close();
-    }
-}
+   class Array{
+   public static void main(){
+       Scanner sc=new Scanner(System.in);
+       int marks[] = {12,14,16,18,20};// Array elements
+       
+       System.out.println(marks[1]);// Accessing element at specific index
+       System.out.println("Update Array Elemets:");
+       marks[3]=100;   ///Update element by their index.
+       
+       System.out.println("Array elements: ");
+       for(int i =0;i<marks.length;i++){
+           System.out.println(marks[i]); //Printing elements
+       }
+       System.out.println("Enter your element: ");
+       int m=sc.nextInt();
+       for(int i=0;i<marks.length;i++){
+           if(marks[i]==m){
+               System.out.println("Number found at idx: "+i); //index specific element
+           }
+   }
+   }
+   }
