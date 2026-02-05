d79de72d09c3202597c990d5400de1971d576c8b
yadavharshit448-png <yadavharshit118@gmail.com>
2026-02-05 14:58:30 +0530

    added String exmp

---
+++ practice-programs/StringExmp.java
@@ -0,0 +1,26 @@
+public class StringExmp {
+    public static void main(String[] args){
+         
+		 String name ="harshit:";
+		 
+		 System.out.println(name.length());
+		 System.out.println(name.toUpperCase());
+		 System.out.println(name.toLowerCase());
+		 System.out.println(name.charAt(3));
+		 
+		 System.out.println(name.substring(5));
+		 System.out.println(name.substring(0,4));
+		 
+		 System.out.println(name.contains("java"));
+		 
+		 
+		 System.out.println(name.equals("java programming")); 
+		 
+		 
+		 
+		 
+		 
+
+
+    }
+}
