624131ae0a9d41fd229252c831485bfd8a1f4ddb
yadavharshit448-png <yadavharshit118@gmail.com>
2026-03-17 09:53:30 +0530

    added Exception handling code

---
+++ Exception_Handling/ArrayException.java
@@ -0,0 +1,22 @@
+public  class ArrayException{
+
+
+public static void main(Stringp[]args){
+
+
+int arr[3]= {1,2,3};
+
+try{
+System.out.println(arr[5]);
+
+
+
+}
+catch(ArrayExceptionIndexOutOfBondException e){
+
+System.out.println("error occured");
+}
+}
+
+
+}
